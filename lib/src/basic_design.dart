3132c1f7d9b03e94c4aca00e0d71bee35314c011
Daniel Cinto Conde <dansolo@MacBook-Pro-de-Dan-Solo.local>
2022-03-17 22:23:49 -0600

    Add buttons section and text

--- lib/src/basic_design.dart
+++ lib/src/basic_design.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 class BasicDesignScreen extends StatelessWidget {
+  final String lorem =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   const BasicDesignScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          Image(image: AssetImage('assets/tree_landscape.jpg')),
-          Title()
+        children: [
+          const Image(image: AssetImage('assets/tree_landscape.jpg')),
+          const Title(),
+          const ButtonSection(),
+          Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: Text(lorem))
         ],
       ),
     );
@@ -48,6 +57,63 @@ class Title extends StatelessWidget {
           const Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const [
+          ButtonContent(
+            buttonIcon: Icons.phone,
+            buttonText: 'CALL',
+          ),
+          ButtonContent(
+            buttonIcon: Icons.navigation,
+            buttonText: 'ROUTE',
+          ),
+          ButtonContent(
+            buttonIcon: Icons.share,
+            buttonText: 'SHARE',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonContent extends StatelessWidget {
+  final IconData buttonIcon;
+  final String buttonText;
+  const ButtonContent({
+    Key? key,
+    required this.buttonIcon,
+    required this.buttonText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          buttonIcon,
+          color: Colors.blue[400],
+          size: 35,
+        ),
+        Text(
+          buttonText,
+          style: TextStyle(color: Colors.blue[400]),
+        )
+      ],
     );
   }
 }
