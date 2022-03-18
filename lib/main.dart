afeb03d89568a271925ac707734355c84a335bd6
Daniel Cinto Conde <dansolo@MacBook-Pro-de-Dan-Solo.local>
2022-03-17 21:51:44 -0600

    Add assets folder, add image, create TitlÃe widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
+import 'package:designs/src/basic_design.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: 'basic_design',
+      routes: {
+        'basic_design': (BuildContext context) => const BasicDesignScreen()
+      },
     );
   }
 }
