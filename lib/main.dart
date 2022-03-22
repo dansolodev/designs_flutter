8f40c541801ed32225e6229221f1e063512569a4
Daniel Cinto Conde <dansolo@MacBook-Pro-de-Dan-Solo.local>
2022-03-21 19:33:23 -0600

    Scroll design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:designs/src/basic_design.dart';
+import 'package:designs/src/scroll_design.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'basic_design',
+      initialRoute: 'scroll_screen',
       routes: {
-        'basic_design': (BuildContext context) => const BasicDesignScreen()
+        'basic_design': (BuildContext context) => const BasicDesignScreen(),
+        'scroll_screen': (_) => const ScrollScreen()
       },
     );
   }
---
+++ lib/src/scroll_design.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+class ScrollScreen extends StatelessWidget {
+  const ScrollScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [
+            0.5,
+            0.5,
+          ],
+          colors: [
+            Color(0xff5EE8C5),
+            Color(0xff30BAD6),
+          ],
+        )),
+        child: PageView(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          children: const [
+            PageOne(),
+            PageTwo(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PageOne extends StatelessWidget {
+  const PageOne({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [
+        Background(),
+        MainContent(),
+      ],
+    );
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      fontSize: 60,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    );
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          const Text(
+            '11°',
+            style: textStyle,
+          ),
+          const Text(
+            'Miércoles',
+            style: textStyle,
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          const Icon(
+            Icons.keyboard_arrow_down,
+            size: 100,
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff30BAD6),
+      height: double.infinity,
+      alignment: Alignment.topCenter,
+      child: const Image(
+        image: AssetImage('assets/scroll-1.png'),
+      ),
+    );
+  }
+}
+
+class PageTwo extends StatelessWidget {
+  const PageTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff30BAD6),
+      child: Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+              backgroundColor: const Color(0xff0098FA),
+              shape: const StadiumBorder()),
+          onPressed: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Text(
+              'Bienvenido',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
